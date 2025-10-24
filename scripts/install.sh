d44dbde79a01115b034a2a8475da60cdd8f5269f
Edward Zhu <96358920+Zhu-E@users.noreply.github.com>
2025-10-23 22:39:41 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -69,7 +69,7 @@ EOC
 EOF
 
 sudo -u "$TARGET_USER" -H tmux new-session -d -s _tpm_bootstrap
-
+sleep 0.5
 sudo -u "$TARGET_USER" -H tmux send-keys -t _tpm_bootstrap "tmux source-file $USER_HOME/.tmux.conf" C-m
 sleep 0.5
 sudo -u "$TARGET_USER" -H tmux send-keys -t _tpm_bootstrap "$USER_HOME/.tmux/plugins/tpm/bin/install_plugins" C-m
