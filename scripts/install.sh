b6944e911ff1b53dafa53971332454178d1eafb8
Edward Zhu <96358920+Zhu-E@users.noreply.github.com>
2025-10-23 23:02:49 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,36 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
+#TMUX SETUP
+TARGET_USER="${SUDO_USER:-$USER}"
+USER_HOME=$(eval echo ~${SUDO_USER:-$USER})
+sudo -u "$TARGET_USER" -H git clone https://github.com/tmux-plugins/tpm "$USER_HOME/.tmux/plugins/tpm"
+sudo -u "$TARGET_USER" -H mkdir -p $USER_HOME/tmux-logs
+
+sudo -u "$TARGET_USER" -H bash <<EOF
+cat > "\$HOME/.tmux.conf" <<EOC
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+set -g @plugin 'tmux-plugins/tmux-logging'
+set -g @logging-path "\$HOME/tmux-logs"
+
+run -b '~/.tmux/plugins/tpm/tpm'
+EOC
+EOF
+
+
+#Download stuff
+sudo -u "$TARGET_USER" -H tmux new-session -d -s _tpm_bootstrap
+sleep 0.5
+sudo -u "$TARGET_USER" -H tmux send-keys -t _tpm_bootstrap "tmux source-file $USER_HOME/.tmux.conf" C-m
+sleep 0.5
+sudo -u "$TARGET_USER" -H tmux send-keys -t _tpm_bootstrap "$USER_HOME/.tmux/plugins/tpm/bin/install_plugins" C-m
+sleep 1
+sudo -u "$TARGET_USER" -H tmux send-keys -t _tpm_bootstrap C-b I
+sleep 1
+
+sudo -u "$TARGET_USER" -H tmux kill-session -t _tpm_bootstrap
+
 sudo wget https://archive.kali.org/archive-keyring.gpg -O /usr/share/keyrings/kali-archive-keyring.gpg
 sudo apt update -y 
 
@@ -49,31 +79,3 @@ sudo apt install docker-compose -y
 sudo ip tuntap add user kali mode tun ligolo
 sudo ip link set ligolo up
 
-
-#TMUX SETUP
-TARGET_USER="${SUDO_USER:-$USER}"
-USER_HOME=$(eval echo ~${SUDO_USER:-$USER})
-sudo -u "$TARGET_USER" -H git clone https://github.com/tmux-plugins/tpm "$USER_HOME/.tmux/plugins/tpm"
-sudo -u "$TARGET_USER" -H mkdir -p $USER_HOME/tmux-logs
-
-sudo -u "$TARGET_USER" -H bash <<EOF
-cat > "\$HOME/.tmux.conf" <<EOC
-set -g @plugin 'tmux-plugins/tpm'
-set -g @plugin 'tmux-plugins/tmux-sensible'
-set -g @plugin 'tmux-plugins/tmux-logging'
-set -g @logging-path "\$HOME/tmux-logs"
-
-run -b '~/.tmux/plugins/tpm/tpm'
-EOC
-EOF
-
-sudo -u "$TARGET_USER" -H tmux new-session -d -s _tpm_bootstrap
-sleep 0.5
-sudo -u "$TARGET_USER" -H tmux send-keys -t _tpm_bootstrap "tmux source-file $USER_HOME/.tmux.conf" C-m
-sleep 0.5
-sudo -u "$TARGET_USER" -H tmux send-keys -t _tpm_bootstrap "$USER_HOME/.tmux/plugins/tpm/bin/install_plugins" C-m
-sleep 1
-sudo -u "$TARGET_USER" -H tmux send-keys -t _tpm_bootstrap C-b I
-sleep 1
-
-sudo -u "$TARGET_USER" -H tmux kill-session -t _tpm_bootstrap
